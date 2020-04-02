c20c4db9258235e8a40493989ec8225ceb1d721d
wangzhngyang <>
2020-04-02 12:11:31 +0800

    完成1027动态规划实现

--- Q1027.Longest-Arithmetic-Sequence/lib.go
+++ Q1027.Longest-Arithmetic-Sequence/lib.go
@@ -1,11 +1,9 @@
 package Q1027_Longest_Arithmetic_Sequence
 
-import "fmt"
-
 // todo 本地可以运行，网上无法通过
 func longestArithSeqLength(A []int) int {
-
-	return violence(A)
+	return dp(A)
+	//return violence(A)
 }
 
 func Max(a, b int) int {
@@ -15,38 +13,45 @@ func Max(a, b int) int {
 	return b
 }
 
-// 暴力求解
-func violence(A []int) int {
-	fmt.Println(A)
+// 时间复杂度：O(n^2)
+//Runtime: 488 ms, faster than 25.93% of Go online submissions for Longest Arithmetic Sequence.
+//Memory Usage: 104.8 MB, less than 100.00% of Go online submissions for Longest Arithmetic Sequence.
+func dp(A []int) int {
 	if len(A) == 2 {
 		return 2
 	}
-	aMap := make(map[int]int)
-	for K, v := range A {
-		if _, ok := aMap[v]; !ok {
-			aMap[v] = K
+	mem, res := make([]map[int]int, len(A)), 1
+	mem[0] = make(map[int]int)
+	for i := 0; i < len(A); i++ {
+		for j := i + 1; j < len(A); j++ {
+			if mem[j] == nil {
+				mem[j] = make(map[int]int)
+			}
+			v := A[j] - A[i]
+			mem[j][v] = Max(mem[i][v]+1, mem[j][v])
+			res = Max(res, mem[j][v])
 		}
 	}
-	var x, y, z, max, maxTemp, yKey int
-	for i := 0; i < len(A)-1; i++ {
-		for j := i + 1; j < len(A); j++ {
-			x, y, yKey, maxTemp = A[i], A[j], j, 2
-			if x == y {
-				continue
-			}
-			z = 2*y - x
-			for {
-				if zKey, ok := aMap[z]; ok && zKey > yKey {
-					x, y, yKey = y, z, zKey
-					z = 2*y - x
-					maxTemp++
-				} else {
-					max = Max(max, maxTemp)
-					break
+	return res + 1
+}
+
+// 暴力求解 O(n^3) 超时
+func violence(A []int) int {
+	if len(A) == 2 {
+		return 2
+	}
+	res := 2
+	for i := 0; i < len(A)-2; i++ {
+		for j := i + 1; j < len(A)-1; j++ {
+			pre, temp, d := j, 2, A[j]-A[i]
+			for k := j + 1; k < len(A); k++ {
+				if A[k]-A[pre] == d {
+					temp++
+					pre = k
 				}
 			}
+			res = Max(res, temp)
 		}
 	}
-	fmt.Println(max)
-	return max
+	return res
 }
